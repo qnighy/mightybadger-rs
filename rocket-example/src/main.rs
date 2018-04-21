1d9dd0480332dec1e4fdba69640c7102100b8861
Masaki Hara <ackie.h.gmai@gmail.com>
2018-04-21 09:12:32 +0900

    Add rocket-example.

---
+++ rocket-example/src/main.rs
@@ -0,0 +1,28 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate honeybadger;
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/ping")]
+fn ping() -> &'static str {
+    "pong"
+}
+
+#[get("/error")]
+fn error() -> &'static str {
+    panic!("/error is requested");
+}
+
+fn main() {
+    honeybadger::install_hook();
+    rocket::ignite()
+        .mount("/", routes![index, ping, error])
+        .attach(honeybadger::rocket_hook::HoneybadgerHook::new())
+        .launch();
+}
