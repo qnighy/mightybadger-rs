fb1d25ccc10d5373365cb52aa3ff5d461ebc21ed
Masaki Hara <ackie.h.gmai@gmail.com>
2018-06-23 22:09:14 +0900

    Add actix-web support.

---
+++ honeybadger-actix-web/examples/server.rs
@@ -0,0 +1,55 @@
+extern crate actix_web;
+
+extern crate honeybadger;
+extern crate honeybadger_actix_web;
+
+#[macro_use]
+extern crate failure;
+
+use actix_web::error::ResponseError;
+use actix_web::http::Method;
+use actix_web::{server, App, Path, Responder};
+use failure::Backtrace;
+use honeybadger_actix_web::HoneybadgerMiddleware;
+
+fn index(_: Path<()>) -> impl Responder {
+    "Hello, world!"
+}
+
+fn ping(_: Path<()>) -> impl Responder {
+    "pong"
+}
+
+#[derive(Debug, Fail)]
+#[fail(display = "MyError")]
+struct MyError(#[cause] std::io::Error, Backtrace);
+
+impl ResponseError for MyError {}
+
+fn error(_: Path<()>) -> actix_web::error::Result<String> {
+    use std::io::Read;
+    let mut f = std::fs::File::open("quux.quux").map_err(|e| MyError(e, Backtrace::new()))?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .map_err(|e| MyError(e, Backtrace::new()))?;
+    Ok(contents)
+}
+
+fn error_panic(_: Path<()>) -> &'static str {
+    panic!("/error_panic is requested");
+}
+
+fn main() {
+    honeybadger::setup();
+
+    server::new(|| {
+        App::new()
+            .middleware(HoneybadgerMiddleware::new())
+            .route("/", Method::GET, index)
+            .route("/ping", Method::GET, ping)
+            .route("/error", Method::GET, error)
+            .route("/error_panic", Method::GET, error_panic)
+    }).bind("localhost:7878")
+        .expect("bind failed")
+        .run();
+}
---
+++ honeybadger-actix-web/src/lib.rs
@@ -0,0 +1,73 @@
+extern crate actix_web;
+extern crate honeybadger;
+#[macro_use]
+extern crate failure;
+
+use std::collections::HashMap;
+
+use actix_web::http::StatusCode;
+use actix_web::middleware::{Middleware, Response};
+use actix_web::{HttpMessage, HttpRequest, HttpResponse};
+use honeybadger::payload::RequestInfo;
+
+pub struct HoneybadgerMiddleware(());
+
+impl HoneybadgerMiddleware {
+    pub fn new() -> Self {
+        HoneybadgerMiddleware(())
+    }
+}
+
+#[derive(Debug, Fail)]
+#[fail(display = "Unknown Error Response: {}", _0)]
+pub struct ErrorStatus(StatusCode);
+
+impl<S> Middleware<S> for HoneybadgerMiddleware {
+    fn response(
+        &self,
+        req: &mut HttpRequest<S>,
+        resp: HttpResponse,
+    ) -> actix_web::error::Result<Response> {
+        let status = resp.status();
+        if !(status == StatusCode::TOO_MANY_REQUESTS || status.is_server_error()) {
+            return Ok(Response::Done(resp));
+        }
+        let request_info = {
+            let mut cgi_data: HashMap<String, String> = HashMap::new();
+            for (name, value) in req.headers().iter() {
+                let name = "HTTP_"
+                    .chars()
+                    .chain(name.as_str().chars())
+                    .map(|ch| {
+                        if ch == '-' {
+                            '_'
+                        } else {
+                            ch.to_ascii_uppercase()
+                        }
+                    })
+                    .collect::<String>();
+                cgi_data.insert(name, String::from_utf8_lossy(value.as_bytes()).to_string());
+            }
+            let url = format!("http://localhost/{}", req.path());
+            let params: HashMap<String, String> = req.query()
+                .iter()
+                .map(|(key, value)| (key.to_string(), value.to_string()))
+                .collect::<HashMap<String, String>>();
+            RequestInfo {
+                url: url,
+                cgi_data: cgi_data,
+                params: params,
+                ..Default::default()
+            }
+        };
+        honeybadger::context::with(&request_info, || {
+            if let Some(error) = resp.error() {
+                honeybadger::notify(error.as_fail());
+            } else {
+                let error = ErrorStatus(status);
+                honeybadger::notify(&error);
+            }
+        });
+        Ok(Response::Done(resp))
+    }
+}
