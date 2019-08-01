208c84b328aa8ea90e04d5a46b37d7060fe21769
Masaki Hara <ackie.h.gmai@gmail.com>
2019-08-01 16:04:35 +0900

    Fix condition

--- .travis/build-impl.sh
+++ .travis/build-impl.sh
@@ -3,10 +3,10 @@ set -ue
 set -o pipefail
 
 PACKAGES=(honeybadger honeybadger-gotham)
-if [[ ${RUSTUP_TOOLCHAIN:-} != "1.34.2" ]]; then
+if [[ ${RUSTUP_TOOLCHAIN:-${TRAVIS_RUST_VERSION:-}} != "1.34.2" ]]; then
   PACKAGES+=(honeybadger-actix-web)
 fi
-if [[ ${RUSTUP_TOOLCHAIN:-} == nightly* || ${TRAVIS_RUST_VERSION:-} == nightly* ]]; then
+if [[ ${RUSTUP_TOOLCHAIN:-${TRAVIS_RUST_VERSION:-}} == nightly* ]]; then
   PACKAGES+=(honeybadger-rocket)
 fi
 
